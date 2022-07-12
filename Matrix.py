9749a401e7194fd8cffaf923252f7626db384069
Miqueas Garcia Gonzalez <coding4canary@gmail.com>
2022-07-12 07:42:06 +0100

    Creación de cabecera y definición del constructor por defecto

---
+++ Matrix.py
@@ -0,0 +1,15 @@
+__author__ = "Miqueas García González"
+__copyright__ = "-"
+__credits__ = ["-"]
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Miqueas García González"
+__email__ = "coding4canary@gmail.com"
+__status__ = "Development"
+
+class Matrix:
+  def __init__(self): #self: reservada para poder instanciar y acceder al objeto
+    self.n_rows = 0
+    self.n_columns = 0
+  
+  pass
